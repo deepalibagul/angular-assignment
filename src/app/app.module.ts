7e3e78c1455a0467f425542a1e4b4137350d5ccc
Mahendra Bagul <bagulm123@gmail.com>
2018-09-01 00:46:17 +0530

    Angular Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
+import { FontawesomeComponent } from './components/fontawesome/fontawesome.component';
+import { MiscellaneousComponent } from './components/miscellaneous/miscellaneous.component';
+import { APIComponent } from './components/api/api.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootstrapComponent,
+    FontawesomeComponent,
+    MiscellaneousComponent,
+    APIComponent
   ],
   imports: [
     BrowserModule
